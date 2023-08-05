de9864f4dd7c6329a6403801baa404a6b60e6c2a
Clarissa Yeo <clarissayeo94@gmail.com>
2023-08-05 16:56:10 +0800

    Created a to-do list without finishing up the styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "tasks#index"
+
+  get "tasks", to: "tasks#index", as: :tasks
+  get "new", to: "tasks#new"
+  get "task/:id", to: "tasks#show", as: :task
+  get "task/:id/edit", to: "tasks#edit", as: :edit
+  post "tasks", to: "tasks#create"
+  patch "/task/:id", to: "tasks#update"
+  delete "/task/:id", to: "tasks#destroy", as: :delete
 end
